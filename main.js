0c48471f6f830adaec3621dc7e69fd3a0336dfa3
Omar El7alwany <94196070+Omar-abd-Aziz@users.noreply.github.com>
2023-03-06 12:00:51 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.querySelector(".download").addEventListener("click",()=>{
 		  .then((data) => {
 			  const AllUrls = data.url;
 			  console.log(data);
-			showAllUrls(AllUrls,data.thumb,data.meta.title);
+			showAllUrls(AllUrls,data.thumb,data.meta.title,data.id);
 
 		})
 		.catch((error) => {
@@ -24,6 +24,8 @@ document.querySelector(".download").addEventListener("click",()=>{
 		});
 
 
+
+		// https://apiyt.com/iframe/?vid=vI2L0ADzziI
 
 		// fetch(`https://api.snappea.com/v1/video/details?url=${link}`, {
 		//   method: 'GET', // or 'PUT'
@@ -46,24 +48,26 @@ document.querySelector(".download").addEventListener("click",()=>{
 
 
 
-function showAllUrls(AllUrls,img,title){
+function showAllUrls(AllUrls,img,title,id){
 	document.querySelector(".LinksDad").innerHTML=`
 	
 	<img src="${img}" alt="" style="width: 80%; margin: 0 auto 30px auto; border-radius: 20px;">
 	<br>
 	<h1 style="text-align: center;">${title}</h1>
 	<br>
+	<iframe rel="nofollow" style="width:200px; height:35px; border:none; border-radius: 6px; display:block;" src="https://apiyt.com/iframe/?vid=${id}"></iframe>
 	<br>
 	
 	`;
 	AllUrls.forEach((e) => {
 
-		if(e.attr.class==""){
+		if(e.attr.class==""&&e.type=="mp4"){
 
 			let x = parseInt(e.filesize);
 			
 			document.querySelector(".LinksDad").innerHTML+=`
 			<div style="margin: 10px 0px; min-width: 200px; display: flex; justify-content: center;">
+
 				<a href="${e.url}" class="Link">${e.quality}p${!isNaN(x)?"_"+parseInt(x/1000000)+"MB":""}.${e.type} ${e.attr.class}</a>
 			</div>
 			`
